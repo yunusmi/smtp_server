29d4dcc66b94ef6aff56386c45203e2cbca84984
Yunus <yunus.mil@mail.ru>
2024-02-28 15:26:13 +0500

    Set up bash script to set up and uninstall smtp server

--- setup_smtp_server.sh
+++ setup_smtp_server.sh
@@ -2,7 +2,7 @@ apt-get update
 echo "Start installing SMTP server"
 apt-get install mailutils
 echo "Введите хост почтового сервера (прим. smtp.example.com):"
-read smtp_hostn
+read smtp_host
 echo "Введите домен почтового сервера (прим. example.com):"
 read domain
 echo "Saving up system settings"
