f01e5a0f26730d3291e388ffd5b1f5a36bc69654
Yunus Ashyrov <yunus.mil@mail.ru>
2024-02-28 15:30:30 +0500

    update bash script

--- setup_smtp_server.sh
+++ setup_smtp_server.sh
@@ -1,6 +1,8 @@
 apt-get update
 echo "Start installing SMTP server"
-apt-get install mailutils
+echo 'debconf debconf/frontend select noninteractive' | sudo tee /etc/apt/apt.conf.d/99debconf-noninteractive
+
+apt-get install -y mailutils
 echo "Введите хост почтового сервера (прим. smtp.example.com):"
 read smtp_host
 echo "Введите домен почтового сервера (прим. example.com):"
