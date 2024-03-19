569da45a5410a3100af1cd62ce73b1a917262cd8
Yunus Ashyrov <yunus.mil@mail.ru>
2024-03-19 22:29:49 +0500

    Updated uninstall server script

--- bash_utils/uninstall_smtp_server.sh
+++ bash_utils/uninstall_smtp_server.sh
@@ -14,6 +14,11 @@ if [ -d "/etc/letsencrypt" ]; then
   rm -rf /etc/letsencrypt
 fi
 
+sed -i '/DOMAIN_NAME/d' /etc/environment
+sed -i '/SMTP_HOST/d' /etc/environment
+sed -i '/HOSTNAME_SSL_PATH/d' /etc/environment
+sed -i '/HOSTNAME_PRIVKEY_PATH/d' /etc/environment
+
 echo 'debconf debconf/frontend select interactive'
 
 echo "SMTP server is removed"
